3779987ed689f97e7d3c1700936a0e9f88fc4985
Lukas Kluft <lukas.kluft@gmail.com>
2017-09-30 11:50:40 +0200

    Added class to handle simple movie databases.

--- cinestats/movie.py
+++ cinestats/movie.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 """Provide datastructures to handel single movies as well as databases.
 """
+import numpy as np
+
+
 __all__ = [
     'Movie',
+    'MovieDatabase',
 ]
 
 
@@ -12,4 +16,28 @@ class Movie:
         self.date = date
 
     def __repr__(self):
-        return self.title
+        return "'{title}'".format(title=self.title)
+
+
+class MovieDatabase(list):
+    @classmethod
+    def from__csv(cls, filename):
+        """Load a movie database from CSV file.
+
+        Parameters:
+            filename (str): Path to CSV file.
+        """
+        # Read the full CSV file into a structured ndarray.
+        database_array = np.genfromtxt(
+            fname=filename,
+            delimiter=',',
+            names=True,  # expect field names in first row.
+            dtype=None,  # automatically determine dtype of fields.
+        )
+
+        # Create Movie object from every row in the array (line in CSV).
+        movie_list = [Movie(title.decode(), date.decode()) for title, date
+                      in zip(database_array['movie'], database_array['date'])]
+
+        # Return actual MovieDatabase.
+        return cls(movie_list)
