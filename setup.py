6d1cf1f3c9e092914418fa01ab8de31b4659662c
Lukas Kluft <lukas.kluft@gmail.com>
2020-03-29 15:35:30 +0200

    Update setuptools configuration

--- setup.py
+++ setup.py
@@ -1,48 +1,46 @@
-# -*- coding: utf-8 -*-
-import sys
-from distutils.core import setup
 from os.path import dirname, join
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
-
-if not sys.version_info >= (3, 5, 1):
-    sys.exit('Only support Python version >=3.5.1.\n'
-             'Found version is {}'.format(sys.version))
 
 # Parse version information from plain ASCII file.
-versionfile = join(dirname(__file__), 'cinestats', 'VERSION')
+versionfile = join(dirname(__file__), "cinestats", "VERSION")
 __version__ = open(versionfile).read().strip()
 
-test_deps = [
-    'flake8',
-    'pytest',
-]
+# Parse long description from README file.
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-    name='cinestats',
-    author='Lukas Kluft',
-    author_email='lukas.kluft@gmail.com',
-    url='https://github.com/lkluft/cinestats',
+    name="cinestats",
+    author="Lukas Kluft",
+    author_email="lukas.kluft@gmail.com",
+    url="https://github.com/lkluft/cinestats",
     version=__version__,
     packages=find_packages(),
-    license='GPL-3.0',
-    description='Analyse and visualise statistics around movies and cinema.',
+    license="GPL-3.0",
+    description="Analyse and visualise statistics around movies and cinema.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     classifiers=[
         # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Intended Audience :: End User/Desktop',
-        'Intended Audience :: Other Audience',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Other Audience",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     include_package_data=True,
+    python_requires=">=3.6",
     install_requires=[
-        'matplotlib',
-        'numpy',
+        "matplotlib",
+        "numpy",
     ],
-    setup_requires=['pytest-runner'],
     extras_require={
-        'tests': test_deps,
-        },
-    tests_require=test_deps,
+        "tests": [
+            "flake8",
+            "pytest",
+        ],
+    },
 )
